f5505490799758a2e4e4c7d73ccda3bf53aee2a0
efikater <eknmta@gmail.com>
2023-02-06 15:22:15 +0200

    deleted un-used imports

--- src/test/java/PagesUtils/CategoryPage.java
+++ src/test/java/PagesUtils/CategoryPage.java
@@ -2,7 +2,6 @@ package PagesUtils;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.locators.RelativeLocator;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
--- src/test/java/PagesUtils/HomePage.java
+++ src/test/java/PagesUtils/HomePage.java
@@ -2,14 +2,12 @@ package PagesUtils;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.locators.RelativeLocator;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 public class HomePage extends BasePage {
